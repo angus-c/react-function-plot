88e5428b7ae2c75b64979529c1a46fe6912d8d0a
angus-c <anguscroll@gmail.com>
2015-11-07 15:14:27 -0800

    auto fit

--- src/lib/plotter.js
+++ src/lib/plotter.js
@@ -1,49 +1,51 @@
 import d3 from 'd3';
 
-const defaultDimensions = {
-  height: 200,
-  width: 200,
-  thickness: 4
-}
+const defaultThickness = 4;
 
 export default class Plotter {
-  constructor(selector = 'body', dimensions = defaultDimensions) {
-    Object.assign(this, {selector, dimensions});
-    this.thicknessCorrection = 1 - this.dimensions.thickness / this.dimensions.height;
+  constructor({selector = 'body', thickness = defaultThickness}) {
+    Object.assign(this, {selector, thickness});
   }
 
   addPath(fn) {
-    const {height, width, thickness} = this.dimensions;
+    this.d3Container = d3.select(this.selector);
+    this.size = Math.min(
+      this.d3Container[0][0].getBoundingClientRect().width,
+      this.d3Container[0][0].getBoundingClientRect().height);
+
+    const {size, thickness} = this;
+
     const lineFunction =
       d3.svg.line()
         .x(i => i)
-        .y(i => height - thickness / 2 - width * fn(i / width) * this.thicknessCorrection)
+        .y(i => size - thickness / 2 - size * fn(i / size) * (1 - thickness / size))
         .interpolate('linear');
 
     const svgContainer =
-      d3.select(this.selector)
+      this.d3Container
         .append('svg')
-        .attr('width', width)
-        .attr('height', height)
+        .attr('width', size)
+        .attr('height', size)
 
     svgContainer.append('path')
-      .attr('d', lineFunction([...new Array(width)].map((_, i) => i)))
+      .attr('d', lineFunction([...new Array(Math.round(size))].map((_, i) => i)))
       .attr('stroke', 'blue')
       .attr('stroke-width', thickness)
       .attr('fill', 'none');
   }
 
   updatePath(fn) {
-    const {height, width, thickness} = this.dimensions;
+    const {size, thickness} = this;
+    
     const lineFunction =
       d3.svg.line()
         .x(i => i)
-        .y(i => height - thickness / 2 - width * fn(i / width) * this.thicknessCorrection)
+        .y(i => size - thickness / 2 - size * fn(i / size) * (1 - thickness / size))
         .interpolate('linear');
 
-    const svgContainer = d3.select(this.selector).transition();
+    const svgContainer = this.d3Container.transition();
 
     svgContainer.select('path')
-      .attr('d', lineFunction([...new Array(width)].map((_, i) => i)))
+      .attr('d', lineFunction([...new Array(Math.round(size))].map((_, i) => i)))
   }
 }
