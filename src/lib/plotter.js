0a3a7ce3f9c8003f8a4fa8deeade6145b2511e88
angus-c <anguscroll@gmail.com>
2015-11-07 16:08:01 -0800

    center rendering

--- src/lib/plotter.js
+++ src/lib/plotter.js
@@ -9,26 +9,25 @@ export default class Plotter {
 
   addPath(fn) {
     this.d3Container = d3.select(this.selector);
-    this.size = Math.min(
-      this.d3Container[0][0].getBoundingClientRect().width,
-      this.d3Container[0][0].getBoundingClientRect().height);
+    
+    // render in a square SVG, adjust margins to center
+    const width = this.d3Container[0][0].getBoundingClientRect().width;
+    const height = this.d3Container[0][0].getBoundingClientRect().height;
+    const marginOffset = Math.abs((width - height) / 2);
+    const marginToAdjust =  width > height ? 'margin-left' : 'margin-top';
+    this.size = Math.min(width, height);
 
     const {size, thickness} = this;
-
-    const lineFunction =
-      d3.svg.line()
-        .x(i => i)
-        .y(i => size - thickness / 2 - size * fn(i / size) * (1 - thickness / size))
-        .interpolate('linear');
 
     const svgContainer =
       this.d3Container
         .append('svg')
         .attr('width', size)
         .attr('height', size)
+        .attr('style', `${marginToAdjust}: ${marginOffset}px`)
 
     svgContainer.append('path')
-      .attr('d', lineFunction([...new Array(Math.round(size))].map((_, i) => i)))
+      .attr('d', this.getLineFunction(fn)([...new Array(Math.round(size))].map((_, i) => i)))
       .attr('stroke', 'blue')
       .attr('stroke-width', thickness)
       .attr('fill', 'none');
@@ -36,16 +35,18 @@ export default class Plotter {
 
   updatePath(fn) {
     const {size, thickness} = this;
-    
-    const lineFunction =
-      d3.svg.line()
-        .x(i => i)
-        .y(i => size - thickness / 2 - size * fn(i / size) * (1 - thickness / size))
-        .interpolate('linear');
 
     const svgContainer = this.d3Container.transition();
 
     svgContainer.select('path')
-      .attr('d', lineFunction([...new Array(Math.round(size))].map((_, i) => i)))
+      .attr('d', this.getLineFunction(fn)([...new Array(Math.round(size))].map((_, i) => i)))
+  }
+  
+  getLineFunction(fn) {
+    const {size, thickness} = this;
+    return d3.svg.line()
+      .x(i => i)
+      .y(i => size - thickness / 2 - size * fn(i / size) * (1 - thickness / size))
+      .interpolate('linear');
   }
 }
