41abe0cfa8c8198f70823000e40f1dc19a1fae5e
angus-c <anguscroll@gmail.com>
2015-11-11 12:31:40 -0800

    delete examples

--- examples/lib/simplifiedEasings.js
+++
@@ -1,7 +0,0 @@
-export default {
-  linear: t => t,
-  easeInQuad: t => t * t,
-  easeOutQuad: t => -1 * t * (t - 2),
-  easeInOutQuad: t => (t *= 2, (t < 1) ? 0.5 * t * t : -0.5 * (--t * (t - 2) - 1)),
-  easeInCubic: t => t * t * t
-}
